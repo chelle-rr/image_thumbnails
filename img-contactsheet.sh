4105590c23051d106161c59fdc32165788bf5761
chelle-rr <62896863+chelle-rr@users.noreply.github.com>
2024-05-21 11:31:25 -0400

    Update img-contactsheet.sh

--- img-contactsheet.sh
+++ img-contactsheet.sh
@@ -15,7 +15,7 @@ fi
 # file_exts=("*.jpg" "*.jpeg" "*.tif" ".*tiff" "*.cr2" "*.dng" "*.gif" "*.heic" "*.nef" "*.png" "*.psd" "*.webp")
 
 # Find all photo files recursively in the directory and put them in an array
-image_list=`find "$image_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.tif" -o -iname "*.tiff" -o -iname "*.cr2" -o -iname "*.dng" -o -iname "*.gif" -o -iname "*.heic" -o -iname "*.nef" -o -iname "*.png" -o -iname "*.psd" -o -iname "*.arw"\)`
+image_list=`find "$image_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.tif" -o -iname "*.tiff" -o -iname "*.cr2" -o -iname "*.dng" -o -iname "*.gif" -o -iname "*.heic" -o -iname "*.nef" -o -iname "*.png" -o -iname "*.psd" -o -iname "*.arw" \)`
 
 # If no video files found, exit and alert
 if [[ -z ${image_list[@]} ]]; then
